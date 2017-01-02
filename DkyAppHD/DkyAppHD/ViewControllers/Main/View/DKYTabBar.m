2e33e5378a282a3adede5291c4d42d53534af6c1
王大虾 <wang_wonderful@foxmail.com>
2017-01-02 22:55:29 +0800

    优化tabbar的实现

--- DkyAppHD/DkyAppHD/ViewControllers/Main/View/DKYTabBar.m
+++ DkyAppHD/DkyAppHD/ViewControllers/Main/View/DKYTabBar.m
@@ -26,7 +26,8 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.itemWidth = [UIApplication sharedApplication].keyWindow.frame.size.width / 4;
+        self.itemWidth = kScreenWidth / 4;
+        self.itemPositioning = UITabBarItemPositioningFill;
     }
     return self;
 }
@@ -35,35 +36,20 @@
 {
     [super layoutSubviews];
     
-    self.itemWidth = [UIApplication sharedApplication].keyWindow.frame.size.width / self.items.count;
-    
-    // 设置所有tabbarButton的frame
-    [self setupAllTabBarButtonsFrame];
-    
-    DLog(@"self.itemWidth = %@",@(self.itemWidth));
-    
     self.selectionIndicatorImage = [self getSelectionIndicatorImage];
-}
-
-- (void)setItems:(NSArray<UITabBarItem *> *)items animated:(BOOL)animated{
-    [super setItems:items animated:animated];
-    
-    if(self.items.count <= 0) return;
-    
-    self.itemWidth = [UIApplication sharedApplication].keyWindow.frame.size.width / self.items.count;
 }
 
 - (void)setSelectedBackgrounColor:(UIColor *)selectedBackgrounColor{
     _selectedBackgrounColor = selectedBackgrounColor;
     
-    if(!selectedBackgrounColor || self.itemWidth == 0 || self.tw_width == 0) return;
+    if(!selectedBackgrounColor || [self tabBarButtonWidth] < 1.0) return;
     
     if(self.landscape){
-        self.landscapeImage = [UIImage imageWithColor:selectedBackgrounColor size:CGSizeMake(self.itemWidth, self.tw_height)];
+        self.landscapeImage = [UIImage imageWithColor:selectedBackgrounColor size:CGSizeMake([self tabBarButtonWidth], self.tw_height)];
         self.portraitImage = nil;
     }else{
         self.landscapeImage = nil;
-        self.portraitImage = [UIImage imageWithColor:selectedBackgrounColor size:CGSizeMake(self.itemWidth, self.tw_height)];
+        self.portraitImage = [UIImage imageWithColor:selectedBackgrounColor size:CGSizeMake([self tabBarButtonWidth], self.tw_height)];
     }
 }
 
@@ -72,65 +58,30 @@
     [self setNeedsLayout];
 }
 
-#pragma private method
-
-- (void)setupAllTabBarButtonsFrame
-{
-    NSInteger index = 0;
-    
-    // 遍历所有的button
-    for (UIView *tabBarButton in self.subviews) {
-        // 如果不是UITabBarButton， 直接跳过
-        if (![tabBarButton isKindOfClass:NSClassFromString(@"UITabBarButton")]) continue;
-        
-        // 根据索引调整位置
-        [self setupTabBarButtonFrame:tabBarButton atIndex:index];
-        
-        // 索引增加
-        index++;
-    }
-}
-
-- (void)setupTabBarButtonFrame:(UIView *)tabBarButton atIndex:(NSInteger)index
-{
-    // 计算button的尺寸
-    CGFloat buttonW = self.tw_width / (self.items.count);
-    CGFloat buttonH = self.tw_height;
-    
-    tabBarButton.tw_width = buttonW;
-    tabBarButton.tw_height = buttonH;
-    tabBarButton.tw_x = buttonW * index;
-    tabBarButton.tw_y = 0;
-    
-    // 计算button的尺寸
-//    CGFloat areaW = self.tw_width / (self.items.count);
-//    CGFloat areaX = areaW * index;
-//    CGFloat areaY = 1;
-//    
-//    CGFloat buttonW = tabBarButton.tw_width;
-//    CGFloat buttonH = tabBarButton.tw_height;
-//    
-//    tabBarButton.tw_width = buttonW;
-//    tabBarButton.tw_height = buttonH;
-//    tabBarButton.tw_x = (areaW - buttonW) / 2.0 + areaX;
-//    tabBarButton.tw_y = areaY;
-}
-
 - (UIImage*)getSelectionIndicatorImage{
     UIColor *color = self.selectedBackgrounColor ? self.selectedBackgrounColor : [UIColor clearColor];
     UIImage *image = nil;
     if(self.landscape){
         if(!self.landscapeImage){
-            self.landscapeImage = [UIImage imageWithColor:color size:CGSizeMake(self.itemWidth, self.tw_height)];
+            self.landscapeImage = [UIImage imageWithColor:color size:CGSizeMake([self tabBarButtonWidth], self.tw_height)];
         }
         image = self.landscapeImage;
     }else{
         if(!self.portraitImage){
-            self.portraitImage = [UIImage imageWithColor:color size:CGSizeMake(self.itemWidth, self.tw_height)];
+            self.portraitImage = [UIImage imageWithColor:color size:CGSizeMake([self tabBarButtonWidth], self.tw_height)];
         }
         image = self.portraitImage;
     }
     return image;
+}
+
+- (CGFloat)tabBarButtonWidth{
+    for (UIView *tabBarButton in self.subviews) {
+        // 如果不是UITabBarButton， 直接跳过
+        if (![tabBarButton isKindOfClass:NSClassFromString(@"UITabBarButton")]) continue;
+        return tabBarButton.size.width;
+    }
+    return CGFLOAT_MIN;
 }
 
 @end
