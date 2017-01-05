a6fdd1da2b892c2b118661763595984ef836771b
王大虾 <wang_wonderful@foxmail.com>
2017-01-05 23:44:37 +0800

    提交代码

--- DkyAppHD/DkyAppHD/ViewControllers/Home/DKYHomeViewController.m
+++ DkyAppHD/DkyAppHD/ViewControllers/Home/DKYHomeViewController.m
@@ -8,7 +8,9 @@
 
 #import "DKYHomeViewController.h"
 
-@interface DKYHomeViewController ()
+@interface DKYHomeViewController ()<iCarouselDelegate,iCarouselDataSource>
+
+@property (nonatomic,weak) iCarousel *iCarousel;
 
 @end
 
@@ -40,6 +42,105 @@
     [self.navigationController.navigationBar tw_hideNavigantionBarBottomLine:YES];
     
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self setupiCarousel];
 }
+
+- (void)setupiCarousel{
+    iCarousel *view = [[iCarousel alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth , kScreenHeight - 20)];
+    [self.view addSubview:view];
+    view.delegate = self;
+    view.dataSource = self;
+    view.bounces = NO;
+    view.pagingEnabled = YES;
+    view.type = iCarouselTypeCustom;
+    view.vertical = YES;
+    self.iCarousel = view;
+}
+
+#pragma mark iCarousel taps
+
+- (void)carousel:(__unused iCarousel *)carousel didSelectItemAtIndex:(NSInteger)index
+{
+    NSLog(@"Tapped view number: %@", @(index));
+}
+
+- (void)carouselCurrentItemIndexDidChange:(__unused iCarousel *)carousel
+{
+    NSLog(@"Index: %@", @(self.iCarousel.currentItemIndex));
+}
+
+#pragma mark - iCarousel代理
+-(UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view{
+    if (view == nil) {
+        view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 384)];
+    }
+    view.backgroundColor = [UIColor randomColor];
+    return view;
+}
+
+-(CATransform3D)carousel:(iCarousel *)carousel itemTransformForOffset:(CGFloat)offset baseTransform:(CATransform3D)transform{
+//    NSLog(@"offset = %lf",offset);
+    static CGFloat max_sacle = 1.0f;
+    static CGFloat min_scale = 0.7f;
+    if (offset <= 1 && offset >= -1) {
+        float tempScale = offset < 0 ? 1+offset : 1-offset;
+        float slope = (max_sacle - min_scale) / 1;
+        
+        CGFloat scale = min_scale + slope*tempScale;
+        transform = CATransform3DScale(transform, 1.0, scale, 1);
+    }else{
+        transform = CATransform3DScale(transform, 1.0, min_scale, 1);
+    }
+    
+    return CATransform3DTranslate(transform, 0.0, offset * self.iCarousel.itemWidth * 0.6, 0.0);
+}
+
+-(NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel{
+    return 4;
+}
+
+- (CGFloat)carousel:(__unused iCarousel *)carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value{
+//    NSLog(@"value = %lf",value);
+    //customize carousel display
+    switch (option)
+    {
+        case iCarouselOptionWrap:
+        {
+            //normally you would hard-code this to YES or NO
+            return NO;
+        }
+        case iCarouselOptionSpacing:
+        {
+            //add a bit of spacing between the item views
+            return 0.0;
+        }
+        case iCarouselOptionFadeMax:
+        {
+            if (self.iCarousel.type == iCarouselTypeCustom)
+            {
+                //set opacity based on distance from camera
+                return value;
+            }
+            return value;
+        }
+        case iCarouselOptionShowBackfaces:
+        case iCarouselOptionRadius:
+        case iCarouselOptionAngle:
+        case iCarouselOptionArc:
+        case iCarouselOptionTilt:
+        case iCarouselOptionCount:
+        case iCarouselOptionFadeMin:
+        case iCarouselOptionFadeMinAlpha:
+        case iCarouselOptionFadeRange:
+        case iCarouselOptionOffsetMultiplier:
+        case iCarouselOptionVisibleItems:
+        {
+            return value;
+        }
+    }
+
+}
+
 
 @end
