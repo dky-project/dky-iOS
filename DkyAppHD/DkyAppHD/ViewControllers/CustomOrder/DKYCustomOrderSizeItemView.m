905c88cef3df8c560a3ab198eb8a4a7d07d1c0ba
王大虾 <wang_wonderful@foxmail.com>
2017-03-12 14:22:27 +0800

    十四、性别、组织下拉框操作，性别值为20同时组织值为236时，下面红框需设置为系统后台返回xwValue值

--- DkyAppHD/DkyAppHD/ViewControllers/CustomOrder/DKYCustomOrderSizeItemView.m
+++ DkyAppHD/DkyAppHD/ViewControllers/CustomOrder/DKYCustomOrderSizeItemView.m
@@ -73,17 +73,15 @@
     
     self.lengthView.textField.enabled = !([madeInfoByProductName.productCusmptcateView.isYcAffix caseInsensitiveCompare:@"Y"] == NSOrderedSame || ([madeInfoByProductName.productMadeInfoView.sizeType caseInsensitiveCompare:@"GD"] == NSOrderedSame && [madeInfoByProductName.productMadeInfoView.ycValue isNotBlank]));
     
-    if(madeInfoByProductName.productMadeInfoView.mDimNew22Id != 131){
+    if(madeInfoByProductName.productMadeInfoView.mDimNew22Id != 131 ||
+       madeInfoByProductName.productMadeInfoView.mDimNew12Id != 55 ||
+       (madeInfoByProductName.productMadeInfoView.mDimNew13Id == 20 &&
+        madeInfoByProductName.productMadeInfoView.mDimNew15Id == 36)){
         [self.bigView.optionsBtn setTitle:madeInfoByProductName.productMadeInfoView.xwValue forState:UIControlStateNormal];
     }else{
         [self.bigView.optionsBtn setTitle:self.bigView.optionsBtn.originalTitle forState:UIControlStateNormal];
     }
     
-    if(madeInfoByProductName.productMadeInfoView.mDimNew12Id != 55){
-        [self.bigView.optionsBtn setTitle:madeInfoByProductName.productMadeInfoView.xwValue forState:UIControlStateNormal];
-    }else{
-        [self.bigView.optionsBtn setTitle:self.bigView.optionsBtn.originalTitle forState:UIControlStateNormal];
-    }
     
     if(madeInfoByProductName.productMadeInfoView.mDimNew12Id == 54||
        madeInfoByProductName.productMadeInfoView.mDimNew12Id == 53||
