830ba86c03bb4bdb0ef21ecbd249ce2b48bf41ef
wang_wonderful <wang_wonderful@foxmail.com>
2017-03-27 15:06:53 +0800

    配套

--- DkyAppHD/DkyAppHD/ViewControllers/CustomOrder/DKYCustomOrderMatchItemView.m
+++ DkyAppHD/DkyAppHD/ViewControllers/CustomOrder/DKYCustomOrderMatchItemView.m
@@ -111,6 +111,7 @@
         [item addObject:model.attribname];
     }
     
+    WeakSelf(weakSelf);
     LCActionSheet *actionSheet = [LCActionSheet sheetWithTitle:sender.extraInfo
                                              cancelButtonTitle:kDeleteTitle
                                                        clicked:^(LCActionSheet *actionSheet, NSInteger buttonIndex) {
@@ -120,12 +121,27 @@
                                                            }else{
                                                                [sender setTitle:sender.originalTitle forState:UIControlStateNormal];
                                                            }
+                                                           [weakSelf actionSheetSelected:0 index:buttonIndex];
                                                        }
                                          otherButtonTitleArray:item];
     actionSheet.scrolling = item.count > 10;
     actionSheet.visibleButtonCount = 10;
     actionSheet.destructiveButtonIndexSet = [NSSet setWithObjects:@0, nil];
     [actionSheet show];
+}
+
+- (void)actionSheetSelected:(NSInteger)tag index:(NSInteger)index{
+    NSArray *models = nil;
+    models = self.customOrderDimList.DIMFLAG_NEW41;
+    
+    // 清除
+    if(index == 0){
+        self.addProductApproveParameter.mDimNew41Id = nil;
+        return;
+    }
+    
+    DKYDimlistItemModel *model = [models objectOrNilAtIndex:index - 1];
+    self.addProductApproveParameter.mDimNew41Id = @([model.ID integerValue]);
 }
 
 #pragma mark - mark
