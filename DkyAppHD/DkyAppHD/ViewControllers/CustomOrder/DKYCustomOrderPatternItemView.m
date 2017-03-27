ca5734446e7887630ebd738e7adc84db2213342f
wang_wonderful <wang_wonderful@foxmail.com>
2017-03-27 14:39:54 +0800

    加穗参数

--- DkyAppHD/DkyAppHD/ViewControllers/CustomOrder/DKYCustomOrderPatternItemView.m
+++ DkyAppHD/DkyAppHD/ViewControllers/CustomOrder/DKYCustomOrderPatternItemView.m
@@ -581,7 +581,15 @@
             models = self.customOrderDimList.DIMFLAG_NEW7;
             break;
         case 4:
+            // 清除
+            if(index == 0){
+                self.addProductApproveParameter.mDimNew37Id = nil;
+                return;
+            }
+            
             models = self.customOrderDimList.DIMFLAG_NEW37;
+            model = [models objectOrNilAtIndex:index - 1];
+            self.addProductApproveParameter.mDimNew37Id = @([model.ID integerValue]);
             break;
         case 5:
             models = self.customOrderDimList.DIMFLAG_NEW38;
