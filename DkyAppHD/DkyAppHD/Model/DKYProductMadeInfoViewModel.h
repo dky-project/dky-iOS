719196fb186de3b078670060bf6f84abcf3f3d58
wang_wonderful <wang_wonderful@foxmail.com>
2017-03-29 14:07:06 +0800

    DKYProductMadeInfoViewModel 整理

--- DkyAppHD/DkyAppHD/Model/DKYProductMadeInfoViewModel.h
+++ DkyAppHD/DkyAppHD/Model/DKYProductMadeInfoViewModel.h
@@ -46,13 +46,21 @@
 @property (nonatomic, assign) NSInteger mDimNew17Id;
 
 /**
- * 加档
+ * 颜色
  */
-@property (nonatomic, assign) NSInteger mDimNew1Id;
+@property (nonatomic, copy) NSString * clrRange;
+
+@property (nonatomic, copy) NSArray *clrRangeArray;
+
 /**
- * 裙类别
+ * 式样
  */
-@property (nonatomic, assign) NSInteger mDimNew3Id;
+@property (nonatomic, assign) NSInteger mDimNew12Id;
+
+/**
+ * 钉[文本框]#扣
+ */
+@property (nonatomic, copy) NSString *dkValue;
 
 /**
  * 钉扣
@@ -71,6 +79,63 @@
 @property (nonatomic, assign) NSInteger mDimNew7Id;
 
 /**
+ * 加穗
+ */
+@property (nonatomic, assign) NSInteger mDimNew37Id;
+
+/**
+ * 裤类别
+ */
+@property (nonatomic, assign) NSInteger mDimNew38Id;
+/**
+ * 开口
+ */
+@property (nonatomic, assign) NSInteger mDimNew39Id;
+/**
+ * 加档
+ */
+@property (nonatomic, assign) NSInteger mDimNew1Id;
+/**
+ * 裙类别
+ */
+@property (nonatomic, assign) NSInteger mDimNew3Id;
+
+/**
+ * 收腰
+ */
+@property (nonatomic, assign) NSInteger mDimNew19Id;
+
+/**
+ * 挂件袖肥
+ */
+@property (nonatomic, assign) NSInteger mDimNew18Id;
+
+
+/**
+ * 尺寸：大
+ * 请选择胸围
+ */
+@property (nonatomic, copy) NSString * xwValue;
+
+/**
+ * 实际袖长上面：长[文本框]，尺寸 长
+ */
+@property (nonatomic, copy) NSString * ycValue;
+
+
+/**
+ * 肩型
+ */
+@property (nonatomic, assign) NSInteger mDimNew22Id;
+
+/**
+ * 肩宽cm
+ */
+@property (nonatomic, copy) NSString * jkValue;
+
+
+
+/**
  * 袖型
  */
 @property (nonatomic, assign) NSInteger mDimNew9Id;
@@ -84,67 +149,12 @@
  * 袖型3
  */
 @property (nonatomic, assign) NSInteger mDimNew9Id3;
-/**
- * 下边
- */
-@property (nonatomic, assign) NSInteger mDimNew10Id;
-/**
- * 式样
- */
-@property (nonatomic, assign) NSInteger mDimNew12Id;
-
-
-
-
 
 /**
- * 挂件袖肥
+ * 袖长cm
  */
-@property (nonatomic, assign) NSInteger mDimNew18Id;
-/**
- * 收腰
- */
-@property (nonatomic, assign) NSInteger mDimNew19Id;
-/**
- * 肩型
- */
-@property (nonatomic, assign) NSInteger mDimNew22Id;
-/**
- * 领型
- */
-@property (nonatomic, assign) NSInteger mDimNew25Id;
-/**
- * 领边
- */
-@property (nonatomic, assign) NSInteger mDimNew26Id;
+@property (nonatomic, copy) NSString * xcValue;
 
-/**
- * 领边层
- */
-@property (nonatomic, assign) NSInteger mDimNew28Id;
-/**
- * 袖口
- */
-@property (nonatomic, assign) NSInteger mDimNew32Id;
-
-/**
- * 加穗
- */
-@property (nonatomic, assign) NSInteger mDimNew37Id;
-
-/**
- * 裤类别
- */
-@property (nonatomic, assign) NSInteger mDimNew38Id;
-/**
- * 开口
- */
-@property (nonatomic, assign) NSInteger mDimNew39Id;
-
-/**
- * 配套
- */
-@property (nonatomic, assign) NSInteger mDimNew41Id;
 
 /**
  * 袖边
@@ -152,24 +162,16 @@
 @property (nonatomic, assign) NSInteger mDimNew45Id;
 
 /**
+ * 袖边cm
+ */
+@property (nonatomic, copy) NSString *xbcValue;
+
+/**
  * 袖边组织
  */
 @property (nonatomic, assign) NSInteger mDimNew46Id;
-/**
- * 颜色
- */
-@property (nonatomic, copy) NSString * clrRange;
 
-@property (nonatomic, copy) NSArray *clrRangeArray;
 
-/**
- * 花型
- */
-@property (nonatomic, strong) NSArray * hxShow;
-/**
- * 肩宽cm
- */
-@property (nonatomic, copy) NSString * jkValue;
 /**
  * 领：完全（领下拉框内的值）
  */
@@ -183,62 +185,32 @@
  * 领:领型（领下拉框内的值）
  */
 @property (nonatomic, copy) NSString *lxt;
-/**
- * 品种json字符串
- */
-@property (nonatomic, copy) NSString * pzJsonstr;
-
-@property (nonatomic, copy) NSArray *pzJsonArray;
-
 
 /**
- * 尺寸类型
+ * 领边尺寸
  */
-@property (nonatomic, copy) NSString * sizeType;
+@property (nonatomic, copy) NSString *lbccValue;
+
 /**
- * 袖长cm
+ * 领边层
  */
-@property (nonatomic, copy) NSString * xcValue;
+@property (nonatomic, assign) NSInteger mDimNew28Id;
+
 /**
- * 袖口cm
+ * 领边
  */
-@property (nonatomic, copy) NSString * xkccValue;
+@property (nonatomic, assign) NSInteger mDimNew26Id;
+
 /**
- * 尺寸：大
- * 请选择胸围
+ * 领型
  */
-@property (nonatomic, copy) NSString * xwValue;
+@property (nonatomic, assign) NSInteger mDimNew25Id;
+
 /**
- * 实际袖长上面：长[文本框]
+ * 粒扣后面的备注
  */
-@property (nonatomic, copy) NSString * ycValue;
-/**
- * 支别json字符串
- */
-@property (nonatomic, copy) NSString * zbJsonstr;
-@property (nonatomic, copy) NSArray *zbJsonArray;
-/**
- * 组织json字符串
- */
-@property (nonatomic, copy) NSString * zxJsonstr;
-@property (nonatomic, copy) NSArray *zxJsonArray;
-/**
- * 针型json字符串
- */
-@property (nonatomic, copy) NSString * zzJsonstr;
-@property (nonatomic, copy) NSArray *zzJsonArray;
-/**
- * 袖边cm
- */
-@property (nonatomic, copy) NSString *xbcValue;
-/**
- * 抽条
- */
-@property (nonatomic, copy) NSString *ct;
-/**
- * 抽条1
- */
-@property (nonatomic, copy) NSString *ct1;
+@property (nonatomic, copy) NSString *lxRemark;
+
 
 /**
  * 其他备注
@@ -256,39 +228,23 @@
  * 其他领边
  */
 @property (nonatomic, copy) NSString *lbqt;
-/**
- * 下边cm
- */
-@property (nonatomic, assign) NSInteger xbccValue;
+
+
 
 /**
- * 其他下边
+ * 花型
  */
-@property (nonatomic, copy) NSString *xbRemark;
+@property (nonatomic, strong) NSArray * hxShow;
 
 /**
- * 领边尺寸
+ * 抽条
  */
-@property (nonatomic, copy) NSString *lbccValue;
+@property (nonatomic, copy) NSString *ct;
+/**
+ * 抽条1
+ */
+@property (nonatomic, copy) NSString *ct1;
 
-/**
- * 口袋（其他）
- */
-@property (nonatomic, copy) NSString *qtkdRemark;
-/**
- * 特殊工艺（其他）
- */
-@property (nonatomic, copy) NSString *qttsgyRemark;
-
-/**
- * 粒扣后面的备注
- */
-@property (nonatomic, copy) NSString *lxRemark;
-
-/**
- * 加注
- */
-@property (nonatomic, copy) NSString *jzValue;
 
 /**
  * 烫珠（勾选框列表）
@@ -301,6 +257,11 @@
 @property (nonatomic, copy) NSArray *kdShow;
 
 /**
+ * 口袋（其他）
+ */
+@property (nonatomic, copy) NSString *qtkdRemark;
+
+/**
  * 附件（勾选框列表）
  */
 @property (nonatomic, copy) NSArray *fjShow;
@@ -311,10 +272,84 @@
 @property (nonatomic, copy) NSArray *tsgyShow;
 
 /**
- * 钉[文本框]#扣
+ * 特殊工艺（其他）
  */
-@property (nonatomic, copy) NSString *dkValue;
+@property (nonatomic, copy) NSString *qttsgyRemark;
 
+
+/**
+ * 下边cm
+ */
+@property (nonatomic, assign) NSInteger xbccValue;
+
+/**
+ * 下边
+ */
+@property (nonatomic, assign) NSInteger mDimNew10Id;
+
+/**
+ * 其他下边
+ */
+@property (nonatomic, copy) NSString *xbRemark;
+
+
+/**
+ * 袖口cm
+ */
+@property (nonatomic, copy) NSString * xkccValue;
+
+/**
+ * 袖口
+ */
+@property (nonatomic, assign) NSInteger mDimNew32Id;
+
+
+/**
+ * 加注
+ */
+@property (nonatomic, copy) NSString *jzValue;
+
+
+/**
+ * 配套
+ */
+@property (nonatomic, assign) NSInteger mDimNew41Id;
+
+
+/**
+ * 展示的图片地址
+ */
 @property (nonatomic, copy) NSString *imgUrl;
+
+
+/**
+ * 尺寸类型
+ */
+@property (nonatomic, copy) NSString * sizeType;
+
+
+/**
+ * 品种json字符串
+ */
+@property (nonatomic, copy) NSString * pzJsonstr;
+@property (nonatomic, copy) NSArray *pzJsonArray;
+
+/**
+ * 支别json字符串
+ */
+@property (nonatomic, copy) NSString * zbJsonstr;
+@property (nonatomic, copy) NSArray *zbJsonArray;
+
+/**
+ * 组织json字符串
+ */
+@property (nonatomic, copy) NSString * zxJsonstr;
+@property (nonatomic, copy) NSArray *zxJsonArray;
+
+/**
+ * 针型json字符串
+ */
+@property (nonatomic, copy) NSString * zzJsonstr;
+@property (nonatomic, copy) NSArray *zzJsonArray;
 
 @end
