51e8fab54f143e1928a14c50d242621068f3edea
wang_wonderful <wang_wonderful@foxmail.com>
2017-08-02 09:46:31 +0800

    推荐模块示意图

--- DkyAppHD/DkyAppHD/ViewControllers/Recommend/DKYRecommendViewController.m
+++ DkyAppHD/DkyAppHD/ViewControllers/Recommend/DKYRecommendViewController.m
@@ -8,7 +8,9 @@
 
 #import "DKYRecommendViewController.h"
 
-@interface DKYRecommendViewController ()
+@interface DKYRecommendViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
+
+@property (nonatomic, weak) UICollectionView *collectionView;
 
 @end
 
@@ -27,6 +29,61 @@
 }
 
 
+#pragma mark - collectionView delegate & datasource
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)collectionView{
+    return 2;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    if (section == 0) return 1;
+    
+    return arc4random() % 100;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView*)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([UICollectionViewCell class]) forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor randomColor];
+    return cell;
+}
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    CGSize size = CGSizeZero;
+    if(indexPath.section == 0 && indexPath.item == 0) {
+        size = CGSizeMake(kScreenWidth - 26 * 2, 9 * (kScreenWidth - 26 * 2) / 20);
+        return size;
+    }
+    
+    
+//    CGFloat width = (kScreenWidth - 5 * 26) / 4;
+    CGFloat width = 160;
+    size = CGSizeMake(width, width);
+    
+    return size;
+}
+
+-(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    UIEdgeInsets insets = UIEdgeInsetsZero;
+    
+    if(section == 0){
+        insets = UIEdgeInsetsMake(28, 0, 0, 0);
+        return insets;
+    }
+    
+    insets = UIEdgeInsetsMake(28, 26, 0, 26);
+    
+    return insets;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+
+}
+
+
+
 #pragma mark - UI
 - (void)commonInit{
     self.view.backgroundColor = [UIColor whiteColor];
@@ -34,6 +91,56 @@
     [self setupCustomTitle:@"推荐"];
     
     [self.navigationController.navigationBar setBackgroundImage:[UIImage imageWithColor:[UIColor colorWithHex:0x2D2D33]] forBarMetrics:UIBarMetricsDefault];
+    
+    [self setupCollectionView];
+}
+
+- (void)setupCollectionView{
+    UICollectionViewFlowLayout *flowLayout=[[UICollectionViewFlowLayout alloc] init];
+    [flowLayout setScrollDirection:UICollectionViewScrollDirectionVertical];
+    UICollectionView *collectionView = [[UICollectionView alloc]initWithFrame:CGRectZero collectionViewLayout:flowLayout];
+    
+    collectionView.delegate = self;
+    collectionView.dataSource = self;
+    collectionView.bounces = YES;
+    collectionView.alwaysBounceVertical = YES;
+    collectionView.showsVerticalScrollIndicator = NO;
+    collectionView.showsHorizontalScrollIndicator = NO;
+    collectionView.backgroundColor = [UIColor colorWithHex:0xEEEEEE];
+    
+    [self.view addSubview:collectionView];
+    
+    [collectionView registerClass:[UICollectionViewCell class]forCellWithReuseIdentifier:NSStringFromClass([UICollectionViewCell class])];
+    
+    WeakSelf(weakSelf);
+    [collectionView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(weakSelf.view.mas_top).with.offset(0);
+        make.left.equalTo(weakSelf.view.mas_left).with.offset(0);
+        make.right.equalTo(weakSelf.view.mas_right).with.offset(0);
+        make.bottom.equalTo(weakSelf.view.mas_bottom).with.offset(0);
+    }];
+    
+    self.collectionView = collectionView;
+    [self setupRefreshControl];
+}
+
+-(void)setupRefreshControl{
+    WeakSelf(weakSelf);
+    MJRefreshNormalHeader *header = [MJRefreshNormalHeader headerWithRefreshingBlock:^(){
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [weakSelf.collectionView.mj_header endRefreshing];
+        });
+    }];
+    header.lastUpdatedTimeKey = [NSString stringWithFormat:@"%@Key",[self class]];
+    self.collectionView.mj_header = header;
+    
+    MJRefreshBackNormalFooter *footer = [MJRefreshBackNormalFooter footerWithRefreshingBlock:^(){
+        [weakSelf.collectionView.mj_footer endRefreshing];
+    }];
+    footer.automaticallyHidden = YES;
+    self.collectionView.mj_footer = footer;
+    
+    [self.collectionView.mj_header beginRefreshing];
 }
 
 - (void)setupCustomTitle:(NSString*)title;
