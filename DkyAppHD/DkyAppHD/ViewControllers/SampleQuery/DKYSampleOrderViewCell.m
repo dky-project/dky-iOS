6fd3192fcf8e40d3bc357f93c8171674e08cb96f
王大虾 <wang_wonderful@foxmail.com>
2017-09-02 22:12:11 +0800

    样衣详情下单，如颜色未点击选择使用默认色，pad下单时显示颜色顺序正常，提交生成订单后颜色自动进行了排序，如3587款

--- DkyAppHD/DkyAppHD/ViewControllers/SampleQuery/DKYSampleOrderViewCell.m
+++ DkyAppHD/DkyAppHD/ViewControllers/SampleQuery/DKYSampleOrderViewCell.m
@@ -241,26 +241,21 @@ static const CGFloat basicItemHeight = 45;
     self.addProductApproveParameter.mDimNew17Id = self.madeInfoByProductName.productMadeInfoView.mDimNew17Id ? @(self.madeInfoByProductName.productMadeInfoView.mDimNew17Id): nil;
     
     //颜色
-    for (DKYDahuoOrderColorModel *model in self.madeInfoByProductName.colorViewList) {
-        for (NSString *selectColor in self.madeInfoByProductName.productMadeInfoView.clrRangeArray) {
+    NSMutableArray *selectedColor = [NSMutableArray array];
+    for (NSString *selectColor in self.madeInfoByProductName.productMadeInfoView.clrRangeArray) {
+        for (DKYDahuoOrderColorModel *model in self.madeInfoByProductName.colorViewList) {
             if([model.colorName isEqualToString:selectColor]){
                 model.selected = YES;
+                [selectedColor addObject:selectColor];
                 break;
             }
         }
     }
     
-    NSMutableArray *selectedColor = [NSMutableArray array];
     for (DKYDahuoOrderColorModel *model in self.madeInfoByProductName.colorViewList) {
         if(model.selected){
             self.addProductApproveParameter.colorValue = @(model.colorId);
             break;
-        }
-    }
-    
-    for (DKYDahuoOrderColorModel *model in self.madeInfoByProductName.colorViewList) {
-        if(model.selected){
-            [selectedColor addObject:model.colorName];
         }
     }
     
