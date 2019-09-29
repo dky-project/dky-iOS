a76cd9636aa69072fe11bb95d81c89ab00967231
王大虾 <wang_wonderful@foxmail.com>
2019-09-29 22:03:33 +0800

    改为内网地址

--- DkyAppHD/DkyAppHD/Network/DKYHttpUrl.h
+++ DkyAppHD/DkyAppHD/Network/DKYHttpUrl.h
@@ -14,7 +14,7 @@
 
 //#define BASE_URL       @"http://60.190.63.14:99/dky-web/"
 
-#define BASE_URL       @"http://116.62.246.199:99/dky-web/"
+//#define BASE_URL       @"http://116.62.246.199:99/dky-web/"
 
 //#define BASE_URL     @"http://192.168.68.155:99/dky-web/"
 
@@ -23,7 +23,7 @@
 //#define BASE_URL     @"http://192.168.3.171:8080/dky-web/"
 //#define BASE_URL     @"http://192.168.3.76:7071/dky-web/"
 
-//#define   BASE_URL     @"http://192.168.68.155:8090/dky-web/"
+#define   BASE_URL     @"http://192.168.68.155:8090/dky-web/"
 
 
 
