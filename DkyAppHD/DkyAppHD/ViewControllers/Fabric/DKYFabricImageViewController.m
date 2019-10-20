c647e730c261c546c544700adb9c3c80b71098ed
王大虾 <wang_wonderful@foxmail.com>
2019-10-20 13:22:56 +0800

    1.面料改成色卡 2.设置当前页颜色

--- DkyAppHD/DkyAppHD/ViewControllers/Fabric/DKYFabricImageViewController.m
+++ DkyAppHD/DkyAppHD/ViewControllers/Fabric/DKYFabricImageViewController.m
@@ -63,7 +63,7 @@ static NSString* const defaultRightBtnTitle = @"返回";
 
 #pragma mark - ui
 - (void)commonInit{
-    self.title = @"面料";
+    self.title = @"色卡";
     //self.edgesForExtendedLayout = UIRectEdgeNone;
     self.automaticallyAdjustsScrollViewInsets = NO;
     [self setupBannerView];
@@ -79,6 +79,7 @@ static NSString* const defaultRightBtnTitle = @"返回";
     cycleScrollView.scrollDirection = UICollectionViewScrollDirectionHorizontal;
     cycleScrollView.autoScroll = NO;
     cycleScrollView.backgroundColor = [UIColor whiteColor];
+    cycleScrollView.currentPageDotColor = [UIColor colorWithHex:0x3C3362];
     self.cycleScrollView = cycleScrollView;
     [self.cycleScrollView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.edges.mas_equalTo(UIEdgeInsetsZero);
